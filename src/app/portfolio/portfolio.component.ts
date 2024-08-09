6d2d7b00a9672c60d652f42d3c0d893cad74e415
Yuvraj Jain <yjain@unc.edu>
2024-08-09 10:46:11 -0400

    Update the home component
    Add in the basic features for the home page.

---
+++ src/app/portfolio/portfolio.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-portfolio',
+  templateUrl: './portfolio.component.html',
+  styleUrls: ['./portfolio.component.css']
+})
+export class PortfolioComponent {
+
+    constructor(private titleService: Title) {
+        this.titleService.setTitle("Yuvraj Jain - Portfolio")
+    }
+}
