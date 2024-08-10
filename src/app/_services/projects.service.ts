290c5b8efec0c57f12ac05483d368ee665869ee5
Yuvraj Jain <yjain@unc.edu>
2024-08-10 15:30:57 -0400

    Add in the projects to the service

--- src/app/_services/projects.service.ts
+++ src/app/_services/projects.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { Project } from '../_models/Project';
+import { Tag } from '../_models/Tag';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProjectsService {
 
-  constructor() { }
+    projects: Project[] = [
+        {id: 0, name: "Sample Angular Project", summary: "Text Description", description: '', projectLink: '', tags: [Tag.ANGULAR, Tag.TYPESCRIPT], pictures: []},
+        {id: 1, name: "Sample Angular Project", summary: "Text Description", description: '', projectLink: '', tags: [Tag.ANGULAR, Tag.REACT], pictures: []},
+        {id: 2, name: "Sample Angular Project", summary: "Text Description", description: '', projectLink: '', tags: [Tag.NODEJS, Tag.ASPNET], pictures: []},
+        {id: 3, name: "Sample Angular Project", summary: "Text Description", description: '', projectLink: '', tags: [Tag.JAVASCRIPT, Tag.POSTGRESQL], pictures: []},
+        {id: 4, name: "Sample Angular Project", summary: "Text Description", description: '', projectLink: '', tags: [Tag.JAVA, Tag.CSHARP], pictures: []},
+        {id: 5, name: "Sample Angular Project", summary: "Text Description", description: '', projectLink: '', tags: [Tag.PYTHON, Tag.CSHARP, Tag.JAVA], pictures: []},
+    ]
+
+    constructor() { }
+
+    getProjects() {
+        return this.projects;
+    }
+
+    getProjectById(id: number) : Project {
+        let project = this.projects.find(project => project.id === id);
+        if (project === undefined) {
+            throw new TypeError('There is no project that matches the id: ' + id)
+        };
+        return project;
+    }
+
+
 }
--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,27 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { Project } from '../_models/Project';
 import { Tag } from '../_models/Tag';
+import { ProjectsService } from '../_services/projects.service';
 
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
   styleUrls: ['./portfolio.component.css']
 })
-export class PortfolioComponent {
+export class PortfolioComponent implements OnInit {
 
-    projects: Project[] = [
-        {id: 0, name: "Sample Angular Project", summary: "Text Description", description: '', projectLink: '', tags: [Tag.ANGULAR, Tag.TYPESCRIPT], pictures: []},
-        {id: 1, name: "Sample Angular Project", summary: "Text Description", description: '', projectLink: '', tags: [Tag.ANGULAR, Tag.REACT], pictures: []},
-        {id: 2, name: "Sample Angular Project", summary: "Text Description", description: '', projectLink: '', tags: [Tag.NODEJS, Tag.ASPNET], pictures: []},
-        {id: 3, name: "Sample Angular Project", summary: "Text Description", description: '', projectLink: '', tags: [Tag.JAVASCRIPT, Tag.POSTGRESQL], pictures: []},
-        {id: 4, name: "Sample Angular Project", summary: "Text Description", description: '', projectLink: '', tags: [Tag.JAVA, Tag.CSHARP], pictures: []},
-        {id: 5, name: "Sample Angular Project", summary: "Text Description", description: '', projectLink: '', tags: [Tag.PYTHON, Tag.CSHARP, Tag.JAVA], pictures: []},
+    projects = {} as Project[];
 
-    ]
-
-    constructor(private titleService: Title) {
+    constructor(
+        private titleService: Title,
+        private projectService: ProjectsService
+    ) {
         this.titleService.setTitle("Yuvraj Jain - Portfolio")
+    }
+    
+    ngOnInit(): void {
+        throw new Error('Method not implemented.');
     }
 
 }
