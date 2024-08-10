7e6d071214a1267cf28ad83773bc32553bdf79ab
Yuvraj Jain <yjain@unc.edu>
2024-08-10 13:03:30 -0400

    Create widget for project cards
    Created project cards and custom model
    for displaying tags when displaying these
    project cards under the Portfolio section.

---
+++ src/app/_models/Tag.ts
@@ -0,0 +1,20 @@
+export class Tag {
+    static readonly ANGULAR = new Tag('Angular', 'red');
+    static readonly TYPESCRIPT = new Tag('TypeScript', 'darkred');
+    static readonly PYTHON = new Tag('Python', 'hotpink');
+    static readonly CSHARP = new Tag('C#', 'green');
+    static readonly JAVA = new Tag('Java', 'orange');
+    static readonly NODEJS = new Tag('Node.JS', 'brown');
+    static readonly ASPNET = new Tag('ASP.NET', 'purple');
+    static readonly JAVASCRIPT = new Tag('JavaScript', 'orange');
+    static readonly REACT = new Tag('React', 'blue');
+    static readonly POSTGRESQL = new Tag('PostgreSQL', '#336791');
+    
+    private constructor(private readonly key: string, public readonly color: string) {
+
+    }
+
+    toString() {
+        return this.key;
+    }
+}
--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { Project } from '../_models/Project';
+import { Tag } from '../_models/Tag';
 
 @Component({
   selector: 'app-portfolio',
@@ -8,7 +10,18 @@ import { Title } from '@angular/platform-browser';
 })
 export class PortfolioComponent {
 
+    projects: Project[] = [
+        {id: 0, name: "Sample Angular Project", summary: "Text Description", description: '', projectLink: '', tags: [Tag.ANGULAR, Tag.TYPESCRIPT], pictures: []},
+        {id: 1, name: "Sample Angular Project", summary: "Text Description", description: '', projectLink: '', tags: [Tag.ANGULAR, Tag.REACT], pictures: []},
+        {id: 2, name: "Sample Angular Project", summary: "Text Description", description: '', projectLink: '', tags: [Tag.NODEJS, Tag.ASPNET], pictures: []},
+        {id: 3, name: "Sample Angular Project", summary: "Text Description", description: '', projectLink: '', tags: [Tag.JAVASCRIPT, Tag.POSTGRESQL], pictures: []},
+        {id: 4, name: "Sample Angular Project", summary: "Text Description", description: '', projectLink: '', tags: [Tag.JAVA, Tag.CSHARP], pictures: []},
+        {id: 5, name: "Sample Angular Project", summary: "Text Description", description: '', projectLink: '', tags: [Tag.PYTHON, Tag.CSHARP, Tag.JAVA], pictures: []},
+
+    ]
+
     constructor(private titleService: Title) {
         this.titleService.setTitle("Yuvraj Jain - Portfolio")
     }
+
 }
